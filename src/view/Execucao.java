3a8ff8763d06f794277305ba620ecf5ce1688c49
lucasgpereira010 <lucasgp010@outlook.com>
2019-06-13 10:18:48 -0300

    Atualização

--- src/view/Execucao.java
+++ src/view/Execucao.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 import model.Professor;
 
 public class Execucao {
-
+//att
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Professor professor = new Professor();
